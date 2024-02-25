02306f31f308fd1b70f291880bff176797a9595d
pertuzdev <antonio.pertuz01@gmail.com>
2024-02-24 20:47:55 -0600

    chore: set up path alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.test.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          '.svg',
+        ],
+        alias: [
+          {
+            '@': './src',
+          },
+        ],
+      },
+    ],
+  ],
 };
