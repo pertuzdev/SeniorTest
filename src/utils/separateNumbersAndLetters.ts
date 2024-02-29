4226e18c4f0faa9eb4cf4aa1084e7efabc045139
pertuzdev <antonio.pertuz01@gmail.com>
2024-02-29 10:58:49 -0600

    chore: take into consideration float numbers to separate from letters

--- src/utils/separateNumbersAndLetters.ts
+++ src/utils/separateNumbersAndLetters.ts
@@ -1,6 +1,6 @@
 export const separateNumberAndLetters = (value: string) => {
   // Using regular expression to match the number and letters
-  const matches = value.match(/^(\d+)([A-Za-z]+)$/);
+  const matches = value.match(/^(\d+(?:\.\d+)?)([A-Za-z]+)$/);
 
   // If there are matches
   if (matches && matches.length === 3) {
